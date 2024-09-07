17d8114aa09c8393c3d861b4cc9cbed8b9f41f36
martinL44 <martinledroit@gmail.com>
2024-09-07 16:30:07 +0200

    fix: docker-compoe, start-up, jar

--- start-up-script/start-up.sh
+++ start-up-script/start-up.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
-
-while ! nc -z hadoop-worker1 8042; do
-  sleep 5
-done
-
-while ! nc -z hadoop-worker2 8042; do
-  sleep 5
-done
-
 bash /root/start-hadoop.sh
-
-hdfs dfs -put input/netflix-data.csv
-
+hdfs dfs -mkdir -p input
+hdfs dfs -put netflix-data.csv input
 spark-submit --class spark.batch.DataCleaner --master yarn /root/netflix_processing.jar input/netflix-data.csv netflix_cleaned # CLEANING DATA
-
 spark-submit --class spark.batch.ContentTypeRepartition --master yarn /root/netflix_processing.jar netflix_cleaned content_type_repartition # CONTENT TYPE REPARTITION
-
 spark-submit --class spark.batch.CountryRepartition --master yarn /root/netflix_processing.jar netflix_cleaned country_repartition # COUNTRY REPARTITION
-
 spark-submit --class spark.batch.YearsRepartition --master yarn /root/netflix_processing.jar netflix_cleaned release_year_repartition # RELEASE YEAR REPARTITION
-
 spark-submit --class spark.batch.DirectorRepartition --master yarn /root/netflix_processing.jar netflix_cleaned director_repartition # DIRECTOR ANALYSIS
-
-spark-submit --class spark.batch.AddedDateRepartition --master yarn /root/netflix_processing.jar netflix_cleaned added_date_repartition # DATE ADDED DISTRIBUTION
-
-python3 /root/bigdata_evaluation.py
-
+# spark-submit --class spark.batch.AddedDateRepartition --master yarn /root/netflix_processing.jar netflix_cleaned added_date_repartition # DATE ADDED DISTRIBUTION
+python3 /root/api/bigdata_evaluation.py
